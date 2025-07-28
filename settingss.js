fc890d7b94e72709327abae485a2e5646844c863
Rangadiv <rangadivsupeshala42@gmail.com>
2025-07-28 19:30:46 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,4 +2,4 @@ module.exports = {
   SESSION_ID: " ",  // malvin~BrUChILY#5vnmxtxPFV_knznege9veZWE5m8eFO33t4RhgEedrU0
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // .
-};
+
